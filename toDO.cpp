dc901561a30eeb5d2d1847cec596094f9db44bef
EiriniGero <irenegerodaki@gmail.com>
2026-02-20 14:09:03 +0200

    added panels

--- toDO.cpp
+++ toDO.cpp
@@ -11,15 +11,32 @@ toDO::toDO():wxFrame(nullptr,wxID_ANY,"TO DO LIST"){
 
     wxMenuBar *bar= new wxMenuBar;
     bar->Append(menu,"Here!");
-    bar->SetBackgroundColour(wxColor(255,103,103));
+    bar->SetBackgroundColour(wxColor(240,128,128));
 
     SetMenuBar(bar);
 
     Bind(wxEVT_MENU, &toDO::Smth, this, ID_smth);
     Bind(wxEVT_MENU, &toDO::Exit, this, wxID_EXIT);
 
-    wxPanel *panelbot= new wxPanel(this,wxID_ANY);
-    panelbot-> SetBackgroundColour(wxColor(250,180,180));
+    wxPanel *panelleft= new wxPanel(this, wxID_ANY, wxDefaultPosition, wxSize(200,100));
+    panelleft -> SetBackgroundColour(wxColor(245,245,220));
+
+    wxPanel *panelright= new wxPanel(this, wxID_ANY,wxDefaultPosition, wxSize(200,100));
+    panelright -> SetBackgroundColour(wxColor(255,228,225));
+
+    wxPanel *panelbot= new wxPanel(this, wxID_ANY, wxDefaultPosition, wxSize(200,100));
+    panelbot -> SetBackgroundColour(wxColor(255,182,193));
+
+    wxBoxSizer *sizerH= new wxBoxSizer(wxHORIZONTAL);
+    wxBoxSizer *sizer= new wxBoxSizer(wxVERTICAL);
+
+    sizerH-> Add(panelleft, 3, wxEXPAND);
+    sizerH->Add(panelright, 2, wxEXPAND);
+    sizer-> Add(sizerH, 2, wxEXPAND);
+    sizer-> Add(panelbot, 1, wxEXPAND);
+
+    this-> SetSizerAndFit(sizer);
+
     wxTextCtrl *text= new wxTextCtrl(panelbot,wxID_ANY);
     //wxTextEntry *textent =new wxTextEntry();
 }
